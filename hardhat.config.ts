877ece9e9494d58c1b2f90c78171de9a68fbe6e9
Prince <athinkeraindian@gmail.com>
2025-10-05 16:08:36 +0000

    point

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-toolbox"; 
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
-  networks: {
-    hardhat: {
-      type: "edr-simulated",
-    }, // local in-memory blockchain
-  },
+  
 };
 
 export default config;
